39582344d6ba7165d62da9b20336da7d4a4b090b
Jama Mohamed <jama3137@gmail.com>
2020-11-20 19:54:07 +0300

    Made business and account no fuzzy search prototype/example

--- mpesa-business-no-detector/src/main/java/com/jama/mpesa_business_no_detector/fuzzySearch/SearchBizNo.kt
+++ mpesa-business-no-detector/src/main/java/com/jama/mpesa_business_no_detector/fuzzySearch/SearchBizNo.kt
@@ -5,10 +5,50 @@ import me.xdrop.fuzzywuzzy.FuzzySearch
 
 class SearchBizNo {
 
+    private val cutOff = 70
+    val foo = listOf(
+        "LIPA NA MEPESA",
+
+                "YWCA-PARKVIEW SUITES",
+
+                "Paybill Number",
+
+                "53540",
+
+                "CLIENT'S NAME - RM NO."
+    )
+
+    private val tillNoQueries = listOf("buy goods till", "buy goods", "till")
+    private val paybillNoQueries = listOf("paybill", "pay bill")
+    private val accountNoQueries = listOf("account", "acc")
+
     fun search() {
-        val foo = listOf("jama", "mohamed", "wardere")
-        val s = FuzzySearch.extractOne("jama", foo)
-        Log.e("jjj", "$s")
+        var businessNoMatchedIndex = -1
+        var accountNoMatchedIndex = -1
+        businessNoMatchedIndex = getMatchedIndex(tillNoQueries)
+        if (businessNoMatchedIndex == -1) {
+            businessNoMatchedIndex = getMatchedIndex(paybillNoQueries)
+            accountNoMatchedIndex = getMatchedIndex(accountNoQueries)
+        }
+        val businessNo = if (businessNoMatchedIndex != -1) foo[businessNoMatchedIndex + 1] else "not found"
+        val accountNo = if (accountNoMatchedIndex != -1) foo[accountNoMatchedIndex + 1] else "not found"
+
+        Log.e("jjj", "business No -> $businessNo and acc no -> $accountNo")
+    }
+
+    private fun fuzzySearchIndex(query: String): Int {
+        val matchIndex = FuzzySearch.extractSorted(query, foo, cutOff).map { it.index }
+        return if (matchIndex.isNotEmpty()) matchIndex[0] else -1
+    }
+
+    private fun getMatchedIndex(queries: List<String>): Int {
+        var matchedIndex = -1
+        queries.forEach {
+            if (matchedIndex == -1) {
+                matchedIndex = fuzzySearchIndex(it)
+            }
+        }
+        return matchedIndex
     }
 
 }
