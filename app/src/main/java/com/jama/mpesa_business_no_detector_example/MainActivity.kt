f858840201daa9261d2c26c2eda2d5f776086ed1
Jama Mohamed <jama3137@gmail.com>
2020-11-20 09:33:56 +0300

    Initial commit

---
+++ app/src/main/java/com/jama/mpesa_business_no_detector_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jama.mpesa_business_no_detector_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
