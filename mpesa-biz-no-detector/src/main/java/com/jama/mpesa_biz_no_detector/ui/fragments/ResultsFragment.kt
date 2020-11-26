0ab8d01607e774d881d0459292d04726ce54a406
Jama Mohamed <jama3137@gmail.com>
2020-11-26 09:23:15 +0300

    [Started] Set up the results ui

--- mpesa-biz-no-detector/src/main/java/com/jama/mpesa_biz_no_detector/ui/fragments/ResultsFragment.kt
+++ mpesa-biz-no-detector/src/main/java/com/jama/mpesa_biz_no_detector/ui/fragments/ResultsFragment.kt
@@ -1,13 +1,16 @@
 package com.jama.mpesa_biz_no_detector.ui.fragments
 
 import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.jama.mpesa_biz_no_detector.R
+import kotlinx.android.synthetic.main.fragment_results.view.*
 
 class ResultsFragment : Fragment() {
 
@@ -24,6 +27,16 @@ class ResultsFragment : Fragment() {
         rootView = inflater.inflate(R.layout.fragment_results, container, false)
 
         Log.e("jjj", "bitmap is ${requireArguments().getParcelable<Bitmap>(BITMAP)}")
+        val bitmap = requireArguments().getParcelable<Bitmap>(BITMAP)
+        rootView.imageView.apply {
+            val shape = ContextCompat.getDrawable(requireContext(), R.drawable.image_view_shape)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                foreground = shape
+            } else {
+                background = shape
+            }
+            setImageBitmap(bitmap)
+        }
 
         return rootView
     }
