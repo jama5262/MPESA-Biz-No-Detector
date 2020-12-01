8e235dab1b91b1e04c36f5f0e4e14f6225547557
Jama Mohamed <jama3137@gmail.com>
2020-12-01 11:08:26 +0300

    [Finished] Minor fix in build gradle app

--- app/src/main/java/com/jama/mpesa_biz_no_detector_example/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.jama.mpesa_biz_no_detector_example
-
-object Constants {
-
-    const val AZURE_VISION_ENDPOINT = BuildConfig.AZURE_VISION_ENDPOINT
-    const val AZURE_VISION_KEY = BuildConfig.AZURE_VISION_KEY
-
-}
